82e418893db0bc7cffa55311d9146a4cfe6fe49c
aochi15 <254475925+aochi15@users.noreply.github.com>
2026-01-22 19:16:27 +0200

    Updated database init.sql

--- database/init.sql
+++ database/init.sql
@@ -1,5 +1,7 @@
 CREATE EXTENSION IF NOT EXISTS pg_trgm;
 
+ALTER DATABASE eneba_database SET pg_trgm.word_similarity_threshold = 0.3;
+
 CREATE TYPE key_type AS ENUM ('EA App', 'Xbox Live', 'eShop', 'Rockstar Games Launcher');
 
 CREATE TYPE platform AS ENUM ('PC', 'Xbox Series X|S', 'Nintendo Switch 2');
@@ -19,6 +21,8 @@ CREATE TABLE IF NOT EXISTS games (
     wishlist_count INT DEFAULT 0,
     CHECK (cashback < price)
 );
+
+CREATE INDEX trgm_idx ON games USING GIST (name gist_trgm_ops);
 
 INSERT INTO games (name, image_file_name, key_type, platform, region, price, discount, cashback, wishlist_count)
 VALUES
