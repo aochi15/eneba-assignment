bd232a0b293822641ec4e31ceaabe803b9a55533
aochi15 <254475925+aochi15@users.noreply.github.com>
2026-01-31 14:17:13 +0200

    Fixed husky

--- back-end/lint-staged.config.ts
+++ back-end/lint-staged.config.ts
@@ -1,8 +1,10 @@
 import { type Configuration } from "lint-staged";
 
 const config: Configuration = {
-  "*.{js,jsx,ts,tsx}": ["eslint", "prettier --check"],
-  "*.{json,yml,yaml,md}": ["eslint", "prettier --check"],
+  "*.{js,jsx,ts,tsx,json,yml,yaml,md}": [
+    "npm run lint:check",
+    "npm run format:check",
+  ],
 };
 
 export default config;
